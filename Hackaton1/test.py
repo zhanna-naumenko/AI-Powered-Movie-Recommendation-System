36b43ac3998f4ccdbe650730aa27f00dc74dadc8
Zhanna <75215351+zhanna-naumenko@users.noreply.github.com>
2026-04-10 20:35:08 +0300

    Update test.py
    Changed the password of MySQL

--- Hackaton1/test.py
+++ Hackaton1/test.py
@@ -7,7 +7,7 @@ config = {
     "host":     "127.0.0.1",
     "port":     3306,
     "user":     "root",
-    "password": "!Zhanna7878036",
+    "password": "***************",
     "database": "movies",
     "raise_on_warnings": True
 }
